77006ffa0b0a868b710745113db4603ac5219959
fragdabble <marcgalang@gmail.com>
2014-01-27 17:55:59 -0600

    Initial Commit

---
+++ FavoritePhotos/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FavoritePhotos
+//
+//  Created by Marcial Galang on 1/27/14.
+//  Copyright (c) 2014 Marc Galang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FavoritePhotos/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FavoritePhotos
+//
+//  Created by Marcial Galang on 1/27/14.
+//  Copyright (c) 2014 Marc Galang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
